2fa2fa26775950af0ede7d4fba57b775c0bb0524
Manuel Trambert <manuel.trambert@epitech.eu>
2016-03-30 13:14:48 +0200

    list

--- cell.mli
+++
@@ -1,4 +0,0 @@
-type cell
-type graph = (cell * cell list) list
-
-val create : bool -> int -> int -> cell
--- manu.ml
+++ manu.ml
@@ -1,20 +1,30 @@
-
-let x = ref 0
-let y = ref 0
-type case = {x : int; y : int}
-let listCase = ref []
-
-let rec fill_case a b =
+let x = ref 3
+let y = ref 3
+type cell = {posx : int; posy : int; idx : int}
+type door = {is_open: bool; first_door_idx : int; second_door_idx : int}
+let listCell = ref []
+let listDoor = ref []
+                   
+let rec fill_cell a b =
   if (b != 0)
   then (if (a != 0)
-        then {x = a; y = b}::fill_case (a - 1) b
-        else fill_case !x (b - 1))
+        then {posx = a - 1; posy = b - 1; idx = a - 1 + (b - 1) * !x}::fill_cell (a - 1) b
+        else fill_cell !x (b - 1))
+  else []
+
+let rec fill_door a b =
+  if (b > 1)
+  then (if (a != 0)
+        then {is_open = false; first_door_idx = ((!x * (b - 1)) + a - !x - 1);
+              second_door_idx = ((!x * (b - 1)) + a - 1)}::fill_door(a - 1) b
+        else fill_door !x (b - 1))
   else []
 
 let main () =
   begin
     x := int_of_string(Sys.argv.(1));
     y := int_of_string(Sys.argv.(2));
-    listCase := fill_case !x !y;
+    listCell := fill_cell !x !y;
+    listDoor := fill_door !x !y;
   end
 ;;
